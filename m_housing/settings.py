31a6d25e97cc29b6bed390d9ba737a34ffac9c08
arnara18 <arnara18@ru.is>
2019-05-07 15:00:13 +0000

    batabase

--- m_housing/settings.py
+++ m_housing/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'm_housing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'kgdlhqvb',
+        'USER': 'kgdlhqvb',
+        'PASSWORD': '0dR14ZMcnE_b9jtXbJFyvGuzNs_-egZu',
+        'HOST': 'balarama.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
