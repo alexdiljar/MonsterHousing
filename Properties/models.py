08066aef3e27dc10c1dd892027f8d4880712356f
arnara18 <arnara18@ru.is>
2019-05-08 14:46:01 +0000

    model classes properties

--- Properties/models.py
+++ Properties/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
+class Cities(models.Model):
+    country = models.CharField(max_length=255)
+    zip = models.IntegerField()
+    city = models.CharField(max_length=255)
+
+
+class Addresses(models.Model):
+    street = models.CharField(max_length=255)
+    house_no = models.IntegerField()
+    C_ID = models.ForeignKey(Cities, on_delete=models.CASCADE)
+
+
+class Tags(models.Model):
+    elevator = models.BooleanField()
+    garage = models.BooleanField()
+    near_bloodbank = models.BooleanField()
+    dungeon = models.BooleanField()
+    seacret_entrence = models.BooleanField()
+
+
+class Types(models.Model):
+    description = models.CharField(max_length=999)
+
+
+class Details(models.Model):
+    T_ID = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    Ty_ID = models.ForeignKey(Types, on_delete=models.CASCADE)
+    size = models.CharField(255)
+    price = models.FloatField()
+    rooms = models.IntegerField()
+    description = models.CharField(999)
+
+
+class Properties(models.Model):
+    pass
