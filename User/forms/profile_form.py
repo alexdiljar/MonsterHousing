29801105789ab6a20ec4a7c2d89d1bf25abf371f
Baby_cake <solrunasta@hotmail.com>
2019-05-10 16:01:52 +0000

    working on form and database insertation for user

--- User/forms/profile_form.py
+++ User/forms/profile_form.py
@@ -3,19 +3,37 @@ from User.models import Profile
 from Properties.models import Cities, Addresses
 
 
+class CitiesForm(ModelForm):
+    class Meta:
+        model = Cities
+        exclude = ['id']
+        widgets = {
+            'country': widgets.Select(attrs={'class': 'form-control'}),
+            'zip': widgets.Select(attrs={'class': 'form-control'}),
+            'city': widgets.Select(attrs={'class': 'form-control'}),
+        }
+
+class AddressesForm(ModelForm):
+    class Meta:
+        model = Addresses
+        exclude = ['id', 'Cities_id']
+        widgets = {
+            'street': widgets.Select(attrs={'class': 'form-control'}),
+            'house_no': widgets.Select(attrs={'class': 'form-control'}),
+        }
+
 
 class ProfileForm(ModelForm):
+    CitiesForm(ModelForm)
+    AddressesForm(ModelForm)
     class Meta:
         model = Profile
-        exclude = ['id', 'user']
+        exclude = ['id', 'user', 'address']
         widgets = {
-        #    'country': widgets.TextInput(attrs={'class': 'form-control'}),
-         #   'zip': widgets.TextInput(attrs={'class': 'form-control'}),
-          #  'city': widgets.TextInput(attrs={'class': 'form-control'}),
-           # 'street': widgets.TextInput(attrs={'class': 'form-control'}),
-            #'house_no': widgets.TextInput(attrs={'class': 'form-control'}),
             'snn': widgets.TextInput(attrs={'class': 'form-control'}),
             'phone': widgets.TextInput(attrs={'class': 'form-control'}),
             'profile_image': widgets.TextInput(attrs={'class': 'form-control'}),
-            'address': widgets.TextInput(attrs={'class': 'form-control'}),
+            'country': widgets.Select(attrs={'class': 'form-control'}),
+            'zip': widgets.Select(attrs={'class': 'form-control'}),
+            'city': widgets.Select(attrs={'class': 'form-control'}),
         }
