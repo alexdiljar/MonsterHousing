eb1dfd448d268a192352527aa06eaebabbbb2508
arnara18 <arnara18@ru.is>
2019-05-14 10:57:53 +0000

    Seller Profile

--- Properties/views.py
+++ Properties/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from Properties.models import Properties
-from . import forms
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # TODO : connect views/ the urls to html files under User in this file
 
 
 # Create your views here.
-# def properties(request):
-#     return HttpResponse('Hello from the properties views')
+def properties(request):
+    return HttpResponse('Hello from the properties views')
 
 
 def catalog_index(request):
@@ -19,24 +19,19 @@ def catalog_index(request):
 
 # /properties/[id]
 def get_property_by_id(request, id):
+    print('in get properties by id')
     return render(request, 'Properties/PropertyDetails.html', {
         'property': get_object_or_404(Properties, pk=id)
     })
 
-
+# /[property id]/seller_profile
 @login_required
-def get_seller_profile(request):
-    return render(request, 'Properties/SellerDetails.html')
+def get_seller_profile(request, id):
+    seller = {User.objects.get(id=id)}
+    # u_properties = {'u_properties': Properties.objects.filter(user=seller.)}
+    return render(request, 'Properties/SellerDetails.html', seller)
 
 
 # This should maybe be in user views, not sure
 def add_new_property(request):
-    if request.method == 'POST':
-        form = forms.CreateProperty(request.POST, request.FILES)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            instance.user = request.user
-
-
-def search_properties(request):
     pass
