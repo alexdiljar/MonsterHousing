5c4398ab461bc8d47b1ea8afbece555ca82ef116
arnara18 <arnara18@ru.is>
2019-05-16 11:25:58 +0000

    Nothing New

--- Transactions/forms/transaction_form.py
+++ Transactions/forms/transaction_form.py
@@ -1,30 +1,23 @@
-# class CustomUserChangeForm(UserChangeForm):
-#     class Meta:
-#         model = User
-#
-#         fields = [
-#             'last_name',
-#             'first_name',
-#             'email'
-#         ]
-#
-#         widgets = {
-#             'last_name': widgets.TextInput(attrs={'class': 'form-control'}),
-#             'first_name': widgets.TextInput(attrs={'class': 'form-control'}),
-#             'email': widgets.TextInput(attrs={'class': 'form-control'})
-#         }
-#
-#
-# class CustomCitiesChangeForm(CitesChangeForm):
-#     class Meta:
-#         model = Cites
-#
-#         fields = [
-#             '',
-#             '',
-#             ''
-#         ]
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.models import User
+from django.forms import ModelForm, widgets
+from User.models import Profile
+from Properties.models import Cities, Addresses
+from django import forms
 
-# Custom change user form, cites, addresses
-# Skoða hvað er hægt að nota í properties forms og user forms
-# Solla ætlaði eitthvað að breyta þeim þannig spjalla við þau með þetta
+
+class UserChangeFormPurchase(UserChangeForm):
+    class Meta:
+        model = User
+
+        fields = [
+            'last_name',
+            'first_name',
+            'email'
+        ]
+
+        widgets = {
+            'last_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'first_name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'email': widgets.TextInput(attrs={'class': 'form-control'})
+        }
--- Transactions/views.py
+++ Transactions/views.py
@@ -3,12 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
-
 from Properties.forms.properties_form import TypesForm, TagsForm, DetailsForm, PropertiesForm
 from User.models import Profile
 from Properties.models import Properties, Addresses, Cities
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from User.forms.profile_form import CustomUserChangeForm, ProfileForm, AddressesForm, CitiesForm, RegisterForm
+from Transactions.forms.transaction_form import UserChangeFormPurchase
 
 
 # Create your views here.
@@ -19,8 +19,7 @@ def buy_property(request,id):
         if user.first_name != '':
             profile = Profile.objects.get(user=request.user)
             return render(request, 'Transactions/Information.html', {
-                'user_form': CustomUserChangeForm(instance=user),
+                'user_form': UserChangeFormPurchase(instance=user),
                 'cities_form': CitiesForm(instance=profile.address.city),
                 'addresses_form': AddressesForm(instance=profile.address),
-                'profile_form': ProfileForm(instance=profile),
             })
