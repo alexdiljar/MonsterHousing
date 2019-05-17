45ae531596f6f67a257fb53a2c848c8694278e72
vignirheidarsson <vignirh16@ru.is>
2019-05-17 17:38:20 +0000

    Hamborgari virkar aftur

--- MonsterHousing/settings.py
+++ MonsterHousing/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'User.apps.UserConfig',
     'Properties.apps.PropertiesConfig',
     'Transactions.apps.TransactionsConfig',
-    'Search.apps.SearchConfig',
     'django_countries',
     'django_filters',
 ]
