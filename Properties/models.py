ff930e5e27f65cca9d7ab7944888b700331f1d33
arnara18 <arnara18@ru.is>
2019-05-17 03:33:18 +0000

    Properties

---
+++ Properties/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Cities(models.Model):
+    country = models.CharField(max_length=255)
+    zip = models.IntegerField()
+    city = models.CharField(max_length=255)
+
+
+class Addresses(models.Model):
+    street = models.CharField(max_length=255)
+    house_no = models.IntegerField()
+    city = models.ForeignKey(Cities, on_delete=models.CASCADE)
+
+
+class Tags(models.Model):
+    elevator = models.BooleanField()
+    garage = models.BooleanField()
+    near_bloodbank = models.BooleanField()
+    dungeon = models.BooleanField()
+    secret_entrance = models.BooleanField()
+
+
+class Types(models.Model):
+    type = models.CharField(max_length=999)
+
+
+class Details(models.Model):
+    tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    type = models.ForeignKey(Types, on_delete=models.CASCADE)
+    size = models.CharField(max_length=255)
+    price = models.FloatField()
+    rooms = models.IntegerField()
+    description = models.CharField(max_length=999)
+    property_image = models.CharField(max_length=999)
+
+
+class Properties(models.Model):
+    address = models.ForeignKey(Addresses, on_delete=models.CASCADE)
+    detail = models.ForeignKey(Details, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
---
+++ Properties/views.py
@@ -0,0 +1,131 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
+from Properties.models import *
+from django.contrib.auth.decorators import login_required
+from Properties.forms.properties_form import *
+from Properties.forms.search_form import SearchForm
+from Properties.filters import PropertiesFilter
+
+
+# TODO : connect views/ the urls to html files under User in this file
+
+
+# Create your views here.
+def properties(request):
+    return HttpResponse('Hello from the properties views')
+
+
+def catalog_index(request):
+    context = {'properties': Properties.objects.all(),  # }
+               'form': SearchForm()}
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            # user inputs
+            country_input = form.cleaned_data['country']
+            zip_input = form.cleaned_data['zip']
+            # Type is multi select will be returned as list must iterate when comparing
+            type_input_list = form.cleaned_data['type']
+            size_input = form.cleaned_data['size']
+            max_price_input = form.cleaned_data['max_price']
+            tags_input = form.cleaned_data['tags']
+            sort_input = form.cleaned_data['sort']
+            rooms_input = form.cleaned_data['rooms']
+            print(country_input, str(zip_input), type_input_list, size_input, max_price_input, tags_input, sort_input)
+
+            for prop in Properties.objects.all():
+                # property values
+                type_id = prop.detail.type_id
+                zip = prop.address.city.zip
+                size = prop.detail.size
+                price = prop.detail.price
+                tag_dungeon = prop.detail.tags.dungeon
+                tag_elevator = prop.detail.tags.elevator
+                tag_garage = prop.detail.tags.garage
+                tag_bb = prop.detail.tags.near_bloodbank
+                tag_se = prop.detail.tags.secret_entrance
+
+                print('type id:' + str(type_id) + 'type:' + str(type) + 'zip: ' + str(zip) + 'size' + str(
+                    size) + 'price' + str(price) + 'tags: ')
+                if country_input == prop.address.city.country:
+                    context = {'property': Properties.objects.get(id=18),
+                               'form': form}
+                for type_input in type_input_list:
+                    print(type_input)
+                    print(type_id)
+                    if int(type_input) == int(type_id):
+                        print(prop.pk)
+                        context = {'property': Properties.objects.get(pk=18),
+                                   'form': form}
+                        return render(request, 'base/Catalog.html', context)
+
+    return render(request, 'base/Catalog.html', context)
+
+
+# /properties/[id]
+def get_property_by_id(request, id):
+    return render(request, 'Properties/PropertyDetails.html', {
+        'property': get_object_or_404(Properties, pk=id)
+    })
+
+
+# /[property id]/seller_profile
+@login_required
+def get_seller_profile(request, id):
+    seller = {'seller': (get_object_or_404(Properties, pk=id)).user,
+              'properties': (Properties.objects.filter(user=(get_object_or_404(Properties, pk=id)).user))}
+    return render(request, 'Properties/SellerDetails.html', seller)
+
+
+def search(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            # user inputs
+            country_input = form.cleaned_data['country']
+            zip_input = form.cleaned_data['zip']
+            # Type is multi select will be returned as list must iterate when comparing
+            type_input_list = form.cleaned_data['type']
+            size_input = form.cleaned_data['size']
+            max_price_input = form.cleaned_data['max_price']
+            tags_input = form.cleaned_data['tags']
+            sort_input = form.cleaned_data['sort']
+            rooms_input = form.cleaned_data['rooms']
+            # print(Properties.objects.filter(
+            #     address__city__zip__icontains=zip_input
+            # ).values_list('address__city__zip', flat=False))
+
+            print(country_input, str(zip_input), type_input_list, size_input, max_price_input, tags_input, sort_input)
+            for prop in Properties.objects.all():
+                # property values
+                type_id = prop.detail.type_id
+                zip = prop.address.city.zip
+                size = prop.detail.size
+                price = prop.detail.price
+                tag_dungeon = prop.detail.tags.dungeon
+                tag_elevator = prop.detail.tags.elevator
+                tag_garage = prop.detail.tags.garage
+                tag_bb = prop.detail.tags.near_bloodbank
+                tag_se = prop.detail.tags.secret_entrance
+
+                print('type id:' + str(type_id) + 'type:' + str(type) + 'zip: ' + str(zip) + 'size' + str(
+                    size) + 'price' + str(price) + 'tags: ')
+                for type_input in type_input_list:
+                    print(type_input)
+                    print(type_id)
+                    if int(type_input) == int(type_id):
+                        print(prop)
+                        context = {'property': Properties.objects.get(id=18),
+                                   'form': form}
+
+                        return render(request, 'Properties/SearchBar.html', context)
+
+    form = SearchForm()
+
+    return render(request, 'Properties/SearchBar.html', {'form': form})
+
+
+def search_view(request):
+    f = PropertiesFilter(request.GET, queryset=Properties.objects.all())
+    return render(request, 'Properties/SearchBar.html', {'filter': f})
